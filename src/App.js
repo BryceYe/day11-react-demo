f7c58530afc63db1676b2c609d91a1decb1d2ec9
BryceYe <YEBR@oocl.com>
2025-09-15 11:10:51 +0800

    refactor: extract routes

--- src/App.js
+++ src/App.js
@@ -2,38 +2,8 @@ import {useReducer} from "react";
 import "./App.css"
 import {todoReducer} from "./reducers/TodoReducer";
 import {TodoContext} from "./contexts/TodoContext";
-import {createBrowserRouter, NavLink, Outlet, RouterProvider} from "react-router";
-import {ErrorPage} from "./pages/ErrorPage";
-import {HomePage} from "./pages/HomePage";
-
-function DefaultLayout() {
-    return <div>
-            <header>
-                <nav>
-                    <ul>
-                        <li><NavLink to={"/"}>Home</NavLink></li>
-                    </ul>
-                </nav>
-            </header>
-            <main>
-                <Outlet/>
-            </main>
-        </div>
-}
-
-const routes = createBrowserRouter([
-    {
-        path:"/",
-        element:<DefaultLayout/>,
-        errorElement:<ErrorPage/>,
-        children:[
-            {
-                path:"/",
-                element: <HomePage />
-            }
-        ]
-    }
-])
+import {RouterProvider} from "react-router";
+import {routes} from "./routes/Routes";
 
 export const initState = [
     {id: 1, text: "This is the first thing I need to do", done: false},

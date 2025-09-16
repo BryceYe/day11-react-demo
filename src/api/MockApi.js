558f15dd7b72666b87c870f5b47f289a765a026e
BryceYe <YEBR@oocl.com>
2025-09-16 10:30:34 +0800

    feat: add interceptor

--- src/api/MockApi.js
+++ src/api/MockApi.js
@@ -1,9 +1,27 @@
 import axios from "axios";
+import {message} from "antd";
 
-export const api = axios.create({
+const api = axios.create({
     baseURL: "https://68c7ac8e5d8d9f514732873d.mockapi.io/",
     headers: {
         "Content-Type": "application/json"
     },
     timeout: 10_000
 })
+
+api.interceptors.response.use(
+    (response) => {
+        return response;
+    },
+    (error) => {
+        const {status} = error.response;
+        if (status === 404) {
+            message
+                .error(error.message, 10_1000)
+                .then(r => {});
+        }
+        return Promise.reject(error);
+    }
+);
+
+export { api }
